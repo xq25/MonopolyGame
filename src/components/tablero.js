876d1b25e079fd56cf7338bacbd61e6b070cdf3a
Daniel Muñoz <danieljulius0926@gmail.com>
2025-09-10 20:08:38 -0500

    Tablero del juego implementado

---
+++ src/components/tablero.js
@@ -0,0 +1,65 @@
+const board = document.getElementById("board");
+const url = "http://127.0.0.1:5000/board";
+
+function makeSquare(tile) { //creamos las casillas
+  const div = document.createElement("div"); // creamos un div para estas en los bordes
+  div.className = "square";
+
+  if (tile.color) {
+    const color = document.createElement("div");// si tiene color, creamos un div para el color y se lo colocamos arriba
+    color.className = "color-bar";// le asignamos la clase
+    color.style.background = tile.color;// le asignamos a la casilla el color que le corresponde(si tiene)
+    div.appendChild(color);
+  }
+
+  const name = document.createElement("div");
+  name.textContent = tile.name; // le asignamos el nombre a la casilla
+  div.appendChild(name);
+
+  return div;
+}
+
+fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    // Solo casillas del tablero (sin comunidad/sorpresa )
+    const tiles = [...data.bottom, ...data.left, ...data.top, ...data.right];
+
+    const grid = Array.from({ length: 11 }, () => Array(11).fill(null));
+    //Se hace una cuadrícula de 11 x 11, toda vacía, para luego ir llenándola con casillas.
+
+    tiles.forEach((tile, i) => {
+      let row, col;
+
+      if (i <= 10) {         // Borde inferior empezando por la esquina inferior derecha
+        row = 10;
+        col = 10 - i; //nos vamos moviendo por columnas 
+      } else if (i <= 20) {  // Borde izquierdo
+        row = 10 - (i - 10);  // nos vamos moviendo por filas de arriba a abajo
+        col = 0;
+      } else if (i <= 30) {  // Borde superior
+        row = 0;
+        col = i - 20; // nos movemos por columnas de izquierda a derecha
+      } else {               // Borde derecho
+        row = i - 30; //nos movemos por filas 
+        col = 10;
+      }
+
+      grid[row][col] = makeSquare(tile);
+    });
+
+    // Pintar tablero
+    grid.forEach((row, rowIndex) => {
+      row.forEach((cell, colIndex) => {
+        // Si hay casilla, la pintamos
+        if (cell) {
+          board.appendChild(cell);
+        } else {
+          // Si estamos en el centro (filas 1-9 y cols 1-9), no pintamos nada
+          if (rowIndex >= 1 && rowIndex <= 9 && colIndex >= 1 && colIndex <= 9) {
+            return; 
+          }       
+        }
+      });
+    });
+  });
