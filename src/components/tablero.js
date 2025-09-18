2b48934b0f222800e166feda961e107cf79204f0
Daniel Muñoz <danieljulius0926@gmail.com>
2025-09-18 02:33:04 -0500

    Implementacion del id y type de la casilla del tablero, Ademas de inicio de implementacion de accion al caer en una casilla dependiendo del id y el type haciendo depuracion mediante la consola

--- src/components/tablero.js
+++ src/components/tablero.js
@@ -4,6 +4,20 @@ const url = "http://127.0.0.1:5000/board";
 function makeSquare(tile) { //creamos las casillas
   const div = document.createElement("div"); // creamos un div para estas en los bordes
   div.className = "square";
+  //Agregamos todo esto al html:SQUAREEEE
+  //no es necesario hacer una funcion ya que desde aca se la podemos asignar(mucho mas rapido jaja)
+  if (tile.id) { // si la casilla tiene id
+    div.id = tile.id; // se lo asignamos al div
+  }
+   // Guardamos el tipo de casilla como atributo
+  if (tile.type) { //si la casilla tiene tipo, se lo asignamos como atributo data-type
+    div.setAttribute('data-type', tile.type);
+  }
+// se le agrega manualmente el nombre a la salida
+  // if (tile.name){
+  //   div.setName('data-name', tile.name)
+  // }
+
 
   if (tile.color) {
     const color = document.createElement("div");// si tiene color, creamos un div para el color y se lo colocamos arriba
@@ -13,7 +27,7 @@ function makeSquare(tile) { //creamos las casillas
   }
 
   const name = document.createElement("div");
-  name.textContent = tile.name; // le asignamos el nombre a la casilla
+  name.textContent = tile.name; // le asignamos el nombre y inyectamos a la casilla en el tablero
   div.appendChild(name);
 
   return div;
@@ -62,4 +76,44 @@ fetch(url)
         }
       });
     });
+
+    //Logica al caer en las casillas dependiendo del type
+    const casilla = document.getElementById('4'); // debe ir como parametro el resultado de los dados
+    const tipo = casilla.getAttribute('data-type');
+    //const nombre = casilla.getName('data-name') (no se si es necesario entrar al nombre)
+    //CAJA COMUNIDAD 
+    if (tipo === 'community_chest'){//tener cuidado respecto al valor de validacion ya que debe ser el mismo del backend
+    // lógica para tomar una carta de comunidad
+      
+    } else if (tipo === 'chance') {
+    // Lógica para carta de suerte
+      
+    } else if (tipo === 'property') {
+      // Lógica para propiedades (comprar, pagar renta, etc.)
+      
+    }else if (tipo === 'special') {
+      if (casilla.id === '20'){
+        console.log("PARQUEO GRATISS")
+      }
+      // Lógica para casillas especiales (salida, cárcel, etc
+    }else if(tipo === 'tax'){
+      // Logica para quitar dinero por impuestos
+      if (casilla.id === "4"){
+        console.log("DAS 200 DE IMPUESTOS")//donde van los mensajes en consola debemos acceder al objeto jugador para hacer la accion correspondiente 
+        
+      }else if (casilla.id === "12"){
+        console.log("Das 50 de impuestos")
+
+      }else if (casilla.id === "28"){
+        console.log("Das 50 de impuestos")
+
+      }else if (casilla.id === "38"){
+        console.log("Das 100 de impuestos")
+
+      }
+      //Aqui ya estarian todos los tax hechos 
+    }
+    else if(tipo === 'railroad'){
+      // logica para los ferrocarriles
+    }
   });
